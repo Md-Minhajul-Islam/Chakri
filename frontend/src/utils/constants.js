cb9e1f3fbe0396dc780a3c2ef18e3b3ab8518afe
Minhaj <minhajulislammuhammad@gmail.com>
2025-12-08 21:38:33 +0600

    test

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,11 @@
-export const USER_API_END_POINT = "http://localhost:5000/user";
-export const JOB_API_END_POINT = "http://localhost:5000/job";
-export const APPLICATION_API_END_POINT = "http://localhost:5000/application";
-export const COMPANY_API_END_POINT = "http://localhost:5000/company";
+// export const USER_API_END_POINT = "http://localhost:5000/user";
+// export const JOB_API_END_POINT = "http://localhost:5000/job";
+// export const APPLICATION_API_END_POINT = "http://localhost:5000/application";
+// export const COMPANY_API_END_POINT = "http://localhost:5000/company";
+
+
+export const USER_API_END_POINT = "https://chakri-io2k.onrender.com/user";
+export const JOB_API_END_POINT = "https://chakri-io2k.onrender.com/job";
+export const APPLICATION_API_END_POINT =
+  "https://chakri-io2k.onrender.com/application";
+export const COMPANY_API_END_POINT = "https://chakri-io2k.onrender.com/company";
